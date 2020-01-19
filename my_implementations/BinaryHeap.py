7fd137cd789e28526237f302df33414f025deffb
JF <jfan9988@gmail.com>
2020-01-19 20:15:02 +1100

    implement binary heap delete

--- my_implementations/BinaryHeap.py
+++ my_implementations/BinaryHeap.py
@@ -17,9 +17,39 @@ class BinaryHeap:
 		self.size += 1
 		self.__swim(self.size)
 
+	def sink(self, key):
+		while key * 2 < self.size:
+			child = key * 2
+			if child < self.size and self.heap[child] < self.heap[child+1]:
+				child += 1 # right child is larger
+			if child < key:
+				break
+			temp = self.heap[child]
+			self.heap[child] = self.heap[key]
+			self.heap[key] = temp
+			key = child
+
+	def deleteMax(self):
+		maxItem = self.heap[1]
+		self.heap[1] = self.heap.pop()
+		self.sink(1)
+		self.size -= 1
+		return maxItem
+
+	def isValid(self):
+		isValid = True
+		for i in range(1,self.size):
+			childKey = i * 2
+			if childKey < self.size and self.heap[childKey] > self.heap[i]:
+					isValid = False
+			if childKey + 1 < self.size and self.heap[childKey + 1] > self.heap[i]:
+					isValid = False	
+		return isValid		
+
 	def show(self):
 		for key in self.heap:
 			print(key, end=' ')
+		print()
 
 bheap = BinaryHeap()
 bheap.insert(2)
@@ -30,4 +60,14 @@ bheap.insert(66)
 bheap.insert(30)
 bheap.insert(21)
 bheap.insert(19)
+bheap.insert(44)
 bheap.show()
+assert bheap.isValid()
+
+bheap.heap[1] = 10
+bheap.sink(1)
+bheap.show()
+assert bheap.isValid()
+
+print("max", bheap.deleteMax())
+assert bheap.isValid()
