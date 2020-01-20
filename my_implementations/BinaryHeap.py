5f96234b986d6992e47f159713908b44fa9298da
Jane Fan <jfan9988@gmail.com>
2020-01-20 17:12:45 +1100

    initial heapsort

--- my_implementations/BinaryHeap.py
+++ my_implementations/BinaryHeap.py
@@ -1,9 +1,10 @@
+# this class creates max heap
 class BinaryHeap:
 	def __init__(self):
 		self.heap = [0]
 		self.size = 0
 
-	def __swim(self, key):
+	def swim(self, key):
 		while key // 2 > 0:
 			parentKey = key // 2
 			if self.heap[key] > self.heap[parentKey]:
@@ -15,7 +16,7 @@ class BinaryHeap:
 	def insert(self, value):
 		self.heap.append(value)
 		self.size += 1
-		self.__swim(self.size)
+		self.swim(self.size)
 
 	def sink(self, key):
 		while key * 2 < self.size:
@@ -30,10 +31,12 @@ class BinaryHeap:
 			key = child
 
 	def deleteMax(self):
+		if self.size <= 0:
+			return -1
 		maxItem = self.heap[1]
 		self.heap[1] = self.heap.pop()
-		self.sink(1)
 		self.size -= 1
+		self.sink(1)
 		return maxItem
 
 	def isValid(self):
@@ -50,6 +53,51 @@ class BinaryHeap:
 		for key in self.heap:
 			print(key, end=' ')
 		print()
+
+# this class creates min heap
+class HeapSort:
+	def __init__(self):
+		self.heap = [0]
+		self.size = 0
+
+	# append without heap ordering
+	def append(self, value):
+		self.heap.append(value)
+		self.size += 1
+
+	def convertToHeap(self):
+		for i in range(len(self.heap) - 1, -1):
+			# exchange last with root
+			temp = self.heap[i]
+			self.heap[i] = self.heap[1]
+			self.heap[1] = temp
+			# sink the new root into place
+			self.sink(1)
+	
+	def isValidHeap(self):
+		# check for min heap condition
+		isValid = True
+		for i in range(1,self.size):
+			childKey = i * 2
+			if childKey < self.size and self.heap[childKey] < self.heap[i]:
+					isValid = False
+			if childKey + 1 < self.size and self.heap[childKey + 1] < self.heap[i]:
+					isValid = False	
+		return isValid	
+
+	def show(self):
+		for key in self.heap:
+			print(key, end=' ')
+		print()
+
+	def sort(self):
+		# broken currently
+		for i in range(len(self.heap) - 1):
+			top = self.deleteMax()
+			print(i, top)
+			assert self.isValidHeap()
+			# hsort.show()
+
 
 bheap = BinaryHeap()
 bheap.insert(2)
@@ -71,3 +119,18 @@ assert bheap.isValid()
 
 print("max", bheap.deleteMax())
 assert bheap.isValid()
+
+hsort = HeapSort()
+hsort.append(2)
+hsort.append(5)
+hsort.append(11)
+hsort.append(7)
+hsort.append(66)
+hsort.append(30)
+hsort.append(21)
+hsort.append(19)
+hsort.append(44)
+hsort.convertToHeap()
+hsort.show()
+assert hsort.isValidHeap()
+# hsort.sort()
