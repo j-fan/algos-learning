41f077d293e2481e8c9ea3a7bc41f65c52207fa0
JF <jfan9988@gmail.com>
2020-01-22 23:17:59 +1100

    3 way quicksort

--- my_implementations/QuickSort.py
+++ my_implementations/QuickSort.py
@@ -16,7 +16,6 @@ def partition(arr, lo, hi):
       if j == lo:
         break
       j-=1
-
     if i >= j: #pointers crossed
       break
     swap(arr, i, j)
@@ -24,12 +23,31 @@ def partition(arr, lo, hi):
   swap(arr, lo, j)
   return j # j marks the end of the left subarray
 
+# 3 way partition
+def partition3(arr, lo, hi):
+  pivot = arr[lo]
+  lt = lo
+  gt = hi
+  i = lo
+  while i <= gt:
+    if arr[i] < pivot:
+      swap(arr, lt, i)
+      lt += 1
+      i += 1
+    elif arr[i] > pivot:
+      swap(arr, gt, i)
+      gt -= 1
+    else:
+      i += 1
+  return (lt-1, gt+1)
+
 def quickSort(arr, lo, hi):
   if hi <= lo:
     return
-  j = partition(arr, lo, hi)
-  quickSort(arr, lo, j)
-  quickSort(arr, j+1, hi)
+  lohi = partition3(arr, lo, hi)
+  quickSort(arr, lo, lohi[0])
+  quickSort(arr, lohi[1], hi)
+
 
 testArr = [5, 2, 6 ,9, 4, 8, 1, 7]
 quickSort(testArr, 0, len(testArr)-1)
